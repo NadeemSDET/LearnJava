812bfb6a884cbb4f6af8b92ba3a1ffac6286250f
NadeemSDET <nadeem.rk2@gmail.com>
2023-08-29 18:45:26 +0400

    init

---
+++ src/Session01/students.java
@@ -0,0 +1,26 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+package Session01;
+
+public class students {
+	
+	String name="Nadeem";
+	int num=10;
+	
+// Nadeem Rn
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nadee\\Desktop\\EclipseFolder\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		/*students obj=new students();
+		String Name=obj.name;
+		int Number=obj.num;
+		System.out.println("Name Is :" + Name);
+		System.out.println("Number is : " + Number);*/
+
+	}
+
+}
